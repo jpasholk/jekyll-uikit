6e21cad2a5f788b44d74e7394e39ea61512940cd
mwoolweaver <m.woolweaver@icloud.com>
2018-05-02 18:42:16 -0500

    script push

---
+++ assets/update.sh
@@ -0,0 +1,3 @@
+  wget https://raw.githubusercontent.com/uikit/uikit/develop/dist/css/uikit.min.css -O ./css/uikit.min.css
+  wget https://raw.githubusercontent.com/uikit/uikit/develop/dist/js/uikit-icons.min.js -O ./js/uikit-icons.min.js
+  wget https://raw.githubusercontent.com/uikit/uikit/develop/dist/js/uikit.min.js -O ./js/uikit.min.js
